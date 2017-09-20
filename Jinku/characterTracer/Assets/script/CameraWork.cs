b476b3f63c60332e16a48cf3325941833ee37a07
JinKu <maplemugcup@gmail.com>
2017-09-21 01:13:23 +0900

    delete test file and push characterTracer(플레이어가 NPC의 일정 범위로 접근하면 NPC가 플레이어를 쫒아오고 범위를 넘어가면 그 자리에 있는 것을 구현함)

---
+++ Jinku/characterTracer/Assets/script/CameraWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour {
+    
+    GameObject Skeleton;
+
+    // Use this for initialization
+    void Start () {
+        Skeleton = GameObject.Find("Skeleton");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Skeleton.transform.position.x-20.6f, Skeleton.transform.position.y + 5.12f, Skeleton.transform.position.z - 20.6f);
+	}
+}
---
+++ Jinku/characterTracer/Assets/script/momyChase.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class momyChase : MonoBehaviour
+{
+    public Transform player;
+    static Animator anim;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+        if (Vector3.Distance(player.position, this.transform.position) < 10 && angle < 90)
+        {
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            anim.SetBool("isDancing", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+            anim.SetBool("isRunning", false);
+
+            if (direction.magnitude > 1.5 && direction.magnitude < 3)
+            {
+                anim.SetBool("isWalking", true);
+                this.transform.Translate(0, 0, 0.05f);
+                anim.SetBool("isDancing", false);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isAttacking", false);
+
+            }
+            else if (direction.magnitude > 3)
+            {
+                this.transform.Translate(0, 0, 0.05f * speed);
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isDancing", false);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isAttacking", false);
+            }
+            else if (direction.magnitude < 1.5)
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isDancing", false);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isDancing", true);
+            anim.SetBool("isAttacking", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+        }
+
+    }
+}
