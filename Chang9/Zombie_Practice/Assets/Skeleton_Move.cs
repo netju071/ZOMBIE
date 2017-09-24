b8b65bc1ef98487da36a3a8f529bf75aad28dbc8
Chang9 <uppletaste@gmail.com>
2017-09-25 05:29:42 +0900

    make directory

---
+++ Chang9/Zombie_Practice/Assets/Skeleton_Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton_Move : MonoBehaviour {
+
+    public Animator anim;
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            anim.Play("Attack");
+        }
+
+        if(Input.GetKeyDown("s"))
+        {
+            anim.Play("Skill");
+        }
+        
+    }
+}
